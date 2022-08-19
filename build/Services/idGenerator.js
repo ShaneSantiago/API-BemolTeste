bdd8118917ee97597477063e37a70fcd78c937cd
ShaneSantiago <shane.santiago1@hotmail.com>
2022-08-19 16:45:55 -0300

    Atualizado

--- build/Services/idGenerator.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const uuid_1 = require("uuid");
-const generateId = () => (0, uuid_1.v4)();
-exports.default = generateId;
-//# sourceMappingURL=idGenerator.js.map
--- build/types.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.usuarioTableName = void 0;
-exports.usuarioTableName = "usuarios_cadastrados";
-//# sourceMappingURL=types.js.map
